46417f35f03fc315c6f07357821b994805681872
Tanya <tatyana.panyutina@gmail.com>
2024-10-22 10:15:25 +0000

    mission_6_1

--- models/trips_age_daily.sql
+++ models/trips_age_daily.sql
@@ -1,20 +1,10 @@
-with
-    date_age_cte as (
-        select
-            date,
-            extract(year from date) - extract(year from u.birth_date) as age,
-            price_rub
-        from
-            {{ref("trips_prep")}} as t
-            inner join scooters_raw.users as u on t.user_id = u.id
-    )
-        select
-            date,
-            age,
-            count(*) as trips,
-            sum(price_rub) as revenue_rub
-        from
-            date_age_cte
-        group by
-            1,
-            2
+select
+    "date",
+    age,
+    count(*) as trips,
+    sum(price_rub) as revenue_rub
+from
+    {{ ref("trips_users") }}
+group by
+    1,
+    2
