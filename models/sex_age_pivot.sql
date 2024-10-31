e9affe9a314936320f32b21b91197930acab8b6b
Tatyana Panyutina <tatyana.panyutina@gmail.com>
2024-10-31 03:54:10 +0000

    mission_11_2

--- models/sex_age_pivot.sql
+++ models/sex_age_pivot.sql
@@ -1,9 +1,9 @@
-trips_usersselect
-  age,
-  {{ dbt_utils.pivot(
-      'age'
-      dbt_utils.pivot("sex", ["M", "F"])
-  ) }}
-from {{ ref('trips_users') }}
-group by age
-order by age
+select
+    age,
+    {{ dbt_utils.pivot("sex", ["M", "F"]) }}
+from
+    {{ ref("trips_users") }}
+group by
+    1
+order by
+    1
